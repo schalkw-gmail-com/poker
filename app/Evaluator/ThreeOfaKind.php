c88c3cb952bee3a152a7c8ccb9ca7211d993c90f
Schalk Wepener <schalkw@gmail.com>
2022-12-18 12:54:59 +0200

    clean up three of a kind

--- app/Evaluator/ThreeOfaKind.php
+++ app/Evaluator/ThreeOfaKind.php
@@ -4,9 +4,7 @@ namespace App\Evaluator;
 
 use App\Interface\Evaluators;
 use App\Classes\Hand;
-use Exception;
-
-use function PHPUnit\Framework\throwException;
+use Illuminate\Support\Facades\Log;
 
 class ThreeOfaKind implements Evaluators
 {
@@ -26,27 +24,27 @@ class ThreeOfaKind implements Evaluators
         return $this->hand->validateHand();
     }
 
-    // three of a kind = 3 cards of same rank plus another.
+    //
 
     /**
+     * three of a kind = 3 cards of same rank plus 2 cards of other ranks.
+     *
      * @return bool
      */
     public function evaluate(): bool{
+        Log::debug(__METHOD__ . ' eof() ');
         $return = false;
-        dump('ssss');
+
         if($this->validHand()) {
             $handRanks = $this->hand->returnRanks();
             $handValues = array_count_values($handRanks);
-
-            dump($handValues);
-            dump(count($handValues));
-            dump(max($handValues));
 
             // the array can contain either 3 or 2 elements, but the max value for the elements is 3
             if ((count($handValues) == 3 || (count($handValues) == 2)) && (max($handValues) == 3)) {
                 $return = true;
             }
         }
+        Log::debug(__METHOD__ . ' eof() ');
         return $return;
     }
 }
